22370f25e04f20cd3357be21320f7dd88fcb6a6c
Daniel Kovacs <kdaniel202@gmail.com>
2022-11-09 09:59:16 +0100

    DataBase first commit

---
+++ src/main/java/com/example/DataBaseWebApp/Database/Student.java
@@ -0,0 +1,30 @@
+package com.example.DataBaseWebApp.Database;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Student {
+
+    @Id
+    @GeneratedValue
+    private long id;
+    private String name;
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
